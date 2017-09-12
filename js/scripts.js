03f322bb2acacd6b2b9cc1172de9b395fc7ee2e7
brandonyip <brandonyip@hotmail.com>
2017-09-11 20:10:49 -0600

    Added back to top button functionality
    - Added a back to top button that disappears and appears depending on the scroll

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,11 @@ $(document).ready(function(){
   //methods in here
   console.log("ready!");
 
+
+
+ //function: onClick
+ //element: hamburger icon
+ //description: hamburger icon click expands or retracts the menu
   $(".icon").on('click', function () {
     var nav = document.getElementById("topnav");
     if(nav.className === "navigationBar") {
@@ -15,4 +20,37 @@ $(document).ready(function(){
       nav.className = "navigationBar";
     }
   });
+
+  //function: onClick
+  //element: backToTop button
+  //description: brings view back to the top
+  //Button fades in and out depending on scroll of page
+  $(window).scroll(function(){
+    if($(this).scrollTop() >= 50) {
+      $('#back-to-top').fadeIn(200);
+    }
+    else {
+      $('#back-to-top').fadeOut(200);
+    }
+  });
+
+  //When the arrow is clicked
+  $('#back-to-top').click(function() {
+    $('body, html').animate({
+      scrollTop:0
+    }, 500);
+  });
+
+  /*
+  $(".backToTop").on('click', function() {
+    var x-coord = 0;
+    var y-coord = 0;
+    window.scrollTo(x-coord, y-coord);
+    $("html, body").animate({scrollTop:$(".container")
+    })
+  });*/
+
+
+
+
 });
